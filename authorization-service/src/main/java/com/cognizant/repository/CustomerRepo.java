e0d423168b14c70dcbca8de3876f4a2d5cb25399
lalitha2719 <56752269+lalitha2719@users.noreply.github.com>
2021-07-05 23:23:22 +0530

    Update CustomerRepo.java

--- authorization-service/src/main/java/com/cognizant/repository/CustomerRepo.java
+++ authorization-service/src/main/java/com/cognizant/repository/CustomerRepo.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import com.cognizant.model.Customer;
 
 /**
- * @author alka0
+ * @author Lalitha Sai Jasti
  * Repository class for storing, fetching and manipulating customer data
  */
 @Repository
