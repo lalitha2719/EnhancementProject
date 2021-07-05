4708a8735052b9f369e9a81db9f80b93727a6204
lalitha2719 <lalithasai.jasti@gmail.com>
2021-07-05 23:00:36 +0530

    Enhancement Project

---
+++ Loans-Management-Microservice/src/main/resources/data.sql
@@ -0,0 +1,25 @@
+insert into customer(customer_id, customer_name, customer_email_id, customer_phone_no, customer_address) values (100001,'lalitha','lalitha@gmail.com','1245789635','vijayawada');
+insert into customer(customer_id, customer_name, customer_email_id, customer_phone_no, customer_address) values(100002,'jahnavi','jahnavi@gmail.com','9874561231','Siliguri');
+insert into customer(customer_id, customer_name, customer_email_id, customer_phone_no, customer_address) values(100003,'navya','navya@gmail.com','4567891232','Darjeeling');
+insert into customer(customer_id, customer_name, customer_email_id, customer_phone_no, customer_address) values(100004,'pranathi','pranathi@gmail.com','1569891232','Hyderabad');
+insert into customer(customer_id, customer_name, customer_email_id, customer_phone_no, customer_address) values(100005,'sanjana','sanjana@gmail.com','3698891232','Bhubaneswaar');
+
+
+ 
+
+ 
+
+insert into loan(loan_product_id,loan_product_name,max_loan_eligible,interest_rate,tenure,collateral_type) values(1001,'Home Loan',1500000,12.5,36,'REAL_ESTATE');
+insert into loan(loan_product_id,loan_product_name,max_loan_eligible,interest_rate,tenure,collateral_type) values(1002,'Home Loan',1000000,13.5,30,'CASH_DEPOSIT');
+insert into loan(loan_product_id,loan_product_name,max_loan_eligible,interest_rate,tenure,collateral_type) values(1003,'Car Loan',700000,11.5,30,'REAL_ESTATE');
+insert into loan(loan_product_id,loan_product_name,max_loan_eligible,interest_rate,tenure,collateral_type) values(1004,'Car Loan',500000,12,24,'CASH_DEPOSIT');
+insert into loan(loan_product_id,loan_product_name,max_loan_eligible,interest_rate,tenure,collateral_type) values(1005,'Education Loan',500000,12,24,'CASH_DEPOSIT');
+
+
+ 
+
+ 
+
+ 
+
+
---
+++ authorization-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user(id, name, password) values (1, 'lalitha', 'admin');
+insert into customer(id,name,password) values (100011,'navya','navya');
+insert into customer(id,name,password) values (100012,'sharat','sharat');
+insert into customer(id,name,password) values (100013,'kavitha','kavitha');
+
